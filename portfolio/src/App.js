3ee098b2b4fc983e5bf5fa527801f3cf26ecae08
Kyle Romero <kgromero@gmail.com>
2017-07-06 14:54:32 -0500

    Remove service worker and fix bug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
           <Navbar.Collapse>
             <Nav pullRight>
               <NavDropdown title="Resume" id="basic-nav-dropdown">
-                <MenuItem href="./kgromero.html">HTML</MenuItem>
-                <MenuItem href="./kgromero.pdf">PDF</MenuItem>
-                <MenuItem href="./kgromero.json">JSON</MenuItem>
+                <MenuItem href="./kgromero.html" target="_blank">HTML</MenuItem>
+                <MenuItem href="./kgromero.pdf" target="_blank">PDF</MenuItem>
+                <MenuItem href="./kgromero.json" target="_blank">JSON</MenuItem>
               </NavDropdown>
               <NavItem href="#tech">Technical Skills</NavItem>
               <NavItem href="#work">Work Experience</NavItem>
