b8d7a4f0e8dd51f45964f9afa24fa25d0ca97411
Kyle Romero <kgromero@gmail.com>
2022-03-31 16:45:53 -0400

    Delete portfolio directory

--- portfolio/resume/generate.sh
+++
@@ -1,7 +0,0 @@
-resume export kgromero.html --theme=stackoverflow
-resume export kgromero.pdf --theme=short
-./ResumeFodder/ResumeFodder export resume.json kgromero.doc ./ResumeFodder/Templates/iconic.xml
-cp kgromero.doc ../public/files/kgromero.doc
-cp kgromero.html ../public/files/kgromero.html
-cp kgromero.pdf ../public/files/kgromero.pdf
-cp resume.json ../public/files/kgromero.json
